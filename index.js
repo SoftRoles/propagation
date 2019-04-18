cd13c5e3799dca8a1d967fa7788eb1965c54740d
Hüseyin Yiğit <yigit.hsyn@gmail.com>
2019-04-18 13:55:20 +0000

    line of sight and radio horizon added

---
+++ index.js
@@ -0,0 +1,4 @@
+
+module.exports.pathLoss = require('./lib/path_loss.js')
+module.exports.lineOfSight = require('./lib/radio_horizon.js').los
+module.exports.radioHorizon = require('./lib/radio_horizon.js').rfhor
